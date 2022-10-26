53a2e56c962f243d78ff46cd95fa367d5b20eb78
hai2k1 <68975387+hai2k1@users.noreply.github.com>
2022-10-26 18:55:19 +0700

    Update Api.php

--- src/System/Admin/Api.php
+++ src/System/Admin/Api.php
@@ -13,30 +13,30 @@ class Api extends \Modules\System\Admin\Expend
     protected function table(): Table
     {
         $table = new Table(new $this->model());
-        $table->title('接口授权');
+        $table->title('Interface authorization');
 
-        $table->action()->button('添加', 'admin.system.api.page')->type('dialog');
+        $table->action()->button('Add', 'admin.system.api.page')->type('dialog');
 
-        $table->column('描述', 'name');
+        $table->column('Name', 'name');
         $table->column('SECRET_ID', 'secret_id');
         $table->column('SECRET_KEY', 'secret_key')->hidden();
 
-        $table->column('状态', 'status')->status([
-            1 => '正常',
-            0 => '禁用'
+        $table->column('Status', 'status')->status([
+            1 => 'Normal',
+            0 => 'Disabled'
         ], [
             1 => 'green',
             0 => 'red'
         ]);
 
-        $column = $table->column('操作')->width(220);
-        $column->link('重置TOKEN', 'admin.system.api.token', ['id' => 'api_id'])->type('ajax', ['method' => 'post']);
-        $column->link('编辑', 'admin.system.api.page', ['id' => 'api_id'])->type('dialog');
-        $column->link('删除', 'admin.system.api.del', ['id' => 'api_id'])->type('ajax');
+        $column = $table->column('operate')->width(220);
+        $column->link('reset token', 'admin.system.api.token', ['id' => 'api_id'])->type('ajax', ['method' => 'post']);
+        $column->link('Edit', 'admin.system.api.page', ['id' => 'api_id'])->type('dialog');
+        $column->link('Deleted', 'admin.system.api.del', ['id' => 'api_id'])->type('ajax');
 
-        $table->filter('描述搜索', 'name', function ($query, $value) {
+        $table->filter('Search', 'name', function ($query, $value) {
             $query->where('name', 'like', '%' . $value . '%');
-        })->text('请输入描述搜索')->quick();
+        })->text('Please enter a description to search')->quick();
 
         return $table;
     }
@@ -46,15 +46,15 @@ class Api extends \Modules\System\Admin\Expend
         $form = new Form(new $this->model());
         $form->dialog(true);
 
-        $form->text('描述', 'name')->verify([
+        $form->text('description', 'name')->verify([
             'required',
         ], [
-            'required' => '请输入接口描述',
+            'required' => 'Please enter an interface description',
         ]);
 
-        $form->radio('状态', 'status', [
-            1 => '启用',
-            0 => '禁用',
+        $form->radio('state', 'status', [
+            1 => 'enable',
+            0 => 'disabled',
         ]);
 
         $form->before(function ($data, $type, $model) {
@@ -73,6 +73,6 @@ class Api extends \Modules\System\Admin\Expend
         $info = $this->model::find($id);
         $info->secret_key = \Str::random(32);
         $info->save();
-        return app_success('重置TOKEN成功');
+        return app_success('Reset TOKEN successfully');
     }
 }
